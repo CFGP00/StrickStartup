fbd113b89ded7e7d6e3f1b9ed97d6b21c8dcbe84
annaandmandy <annaandtommy1004@gmail.com>
2025-05-08 16:03:37 -0400

    add about page and project page

--- myportfolio/myportfolio/urls.py
+++ myportfolio/myportfolio/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from projects.views import home
 from projects.views import contact
+from projects.views import about
+from projects.views import projects
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
     path('contact/', contact),
+    path('projects/', projects),
+    path('about/', about),
 ]
 
--- myportfolio/projects/views.py
+++ myportfolio/projects/views.py
@@ -16,5 +16,19 @@ def home(request):
 def contact(request):
     return render(request, "projects/contact.html")
 
+def about(request):
+    return render(request, "projects/about.html")
 
+def projects(request):
+    projects = [
+        {"title": "XX County Transcription of Deeds", "desc": "Leveraged a powerful computer vision model to transcribe 300 year old documents into a digitized format. This data was then indexed, making search much more efficient."},
+        {"title": "Radiologist Level Computer Vision Model", "desc": "We developed and trained a Computer Vision Model to read chest X-Rays, with an accuracy of x.xx %. This model has been made publically available in order to help trained professionals to be able to diagnose patients more efficiently. ", "link" : "https://huggingface.co/spaces/cfgpp/Danny_Net_Demo"}
+    ]
+    about = "We are a team of Data Science Consultants eager to work on varying projects. From Healthcare to Computer Vision, Strick Technologies is here to help. Contact us to collaborate or consult!"
+    contact_email = "your@email.com"
+    return render(request, "projects/projects.html", {
+        "projects": projects,
+        "about": about,
+        "contact_email": contact_email
+    })
 
